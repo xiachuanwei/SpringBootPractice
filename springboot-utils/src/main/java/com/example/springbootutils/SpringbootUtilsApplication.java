67839925f2ae11dba23d08d9b91be68a88d2a8f4
xiaweiwei <@qingmap.cn>
2019-11-07 09:57:37 +0800

    比较好用的工具类

--- springboot-utils/src/main/java/com/example/springbootutils/SpringbootUtilsApplication.java
+++ springboot-utils/src/main/java/com/example/springbootutils/SpringbootUtilsApplication.java
@@ -10,8 +10,8 @@ public class SpringbootUtilsApplication {
 
     public static void main(String[] args) {
 //        SpringApplication.run(SpringbootUtilsApplication.class, args);
-        DBInfo dbInfo = new DBInfo("com.mysql.cj.jdbc.Driver", "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=utf-8&allowMultiQueries=true", "root", "root");
-        System.out.println(DatabaseConnTool.querySql("select * from student", dbInfo).size());
+        DBInfo dbInfo = new DBInfo("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=utf-8&allowMultiQueries=true", "root", "root");
+        System.out.println(DatabaseConnTool.querySql("select * from test", dbInfo).size());
     }
 
 }
